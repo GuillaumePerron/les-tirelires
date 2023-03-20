cb28f29deb3c02554c770114b451f902f66e92f4
Perron Guillaume <guillaume.perron@etudiant.unc.nc>
2023-03-20 17:41:33 +1100

    Changement

--- src/app/boite/boite.component.ts
+++ src/app/boite/boite.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-boite',
+  selector: 'boite',
   templateUrl: './boite.component.html',
   styleUrls: ['./boite.component.css']
 })
--- src/app/tirelire/tirelire.component.ts
+++ src/app/tirelire/tirelire.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tirelire',
+  selector: 'tirelire',
   templateUrl: './tirelire.component.html',
   styleUrls: ['./tirelire.component.css']
 })
