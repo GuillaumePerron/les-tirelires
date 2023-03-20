47e07dc89d879533c6d15a040a907b56e9c7bb71
Perron Guillaume <guillaume.perron@etudiant.unc.nc>
2023-03-20 17:35:34 +1100

    Les composants

---
+++ src/app/tirelire/tirelire.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tirelire',
+  templateUrl: './tirelire.component.html',
+  styleUrls: ['./tirelire.component.css']
+})
+export class TirelireComponent {
+
+}
